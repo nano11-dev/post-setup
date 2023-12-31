005737c348c578e27028fc2b5b039c98b44133ca
BunnyRabbit12 <155304793+BunnyRabbit12@users.noreply.github.com>
2023-12-31 12:57:21 +0000

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,4 @@
+mkdir C:\Windows\Temp\N11\
+mkdir C:\Users\Public\Desktop\Post-Setup
+irm https://codeload.github.com/nano11-dev/post-setup/zip/refs/heads/main -OutFile C:\Windows\Temp\N11\N11-main.zip
+irm https://www.7-zip.org/a/7zr.exe -OutFile C:\Windows\Temp\N11\7z.exe
