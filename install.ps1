c204e1bc2fb3461257b7ae7e52b2f312946964c7
BunnyRabbit12 <155304793+BunnyRabbit12@users.noreply.github.com>
2023-12-31 13:12:58 +0000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,4 +4,4 @@ irm https://codeload.github.com/nano11-dev/post-setup/zip/refs/heads/main -OutFi
 irm https://raw.githubusercontent.com/BunnyRabbit12/N11-post-setup/BIN/7zxa.dll -OutFile C:\Windows\Temp\N11\7zxa.dll
 irm https://github.com/BunnyRabbit12/N11-post-setup/raw/BIN/7za.exe -OutFile C:\Windows\Temp\N11\7za.exe
 C:\Windows\Temp\N11\7za.exe e C:\Windows\Temp\N11\N11-main.zip -y -oc:C:\Users\Public\Desktop\Post-Setup\
-rmdir C:\Windows\Temp\N11\ /s /q
+rd c:\Windows\Temp\N11 -Recurse -force
