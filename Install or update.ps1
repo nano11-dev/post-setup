b166ceb98d54c04cdef26f795348d2a5fe01fd16
XPower7125 <64551044+XPower7125@users.noreply.github.com>
2024-02-09 16:10:08 +0100

    Update Install or update.ps1

--- Install or update.ps1
+++ Install or update.ps1
@@ -6,7 +6,7 @@ irm https://codeload.github.com/nano11-dev/post-setup/zip/refs/heads/main -OutFi
 irm https://raw.githubusercontent.com/BunnyRabbit12/N11-post-setup/BIN/7zxa.dll -OutFile C:\Windows\Temp\N11\7zxa.dll
 irm https://github.com/BunnyRabbit12/N11-post-setup/raw/BIN/7za.exe -OutFile C:\Windows\Temp\N11\7za.exe
 C:\Windows\Temp\N11\7za.exe x C:\Windows\Temp\N11\N11-main.zip -y -oc:\Users\Public\Desktop\
-cmd.exe /c del /s /q C:\users\public\desktop\post-setup
+cmd.exe /c rmdir /s /q C:\users\public\desktop\post-setup
 ren "C:\Users\Public\Desktop\post-setup-main" "C:\Users\Public\Desktop\post-setup"
 cmd.exe /c rmdir c:\Windows\temp\N11\ /s /q
 exit
